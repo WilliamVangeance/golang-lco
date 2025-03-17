c58c08fc5d417e7e20416d0ce1988200ab57ada4
google <kabirsingh@gmail.com>
2025-03-17 11:26:35 +0530

    going with the basics

--- 02variables/main.go
+++ 02variables/main.go
@@ -11,4 +11,10 @@ func main() {
 	fmt.Println(username)
 	fmt.Printf("variable username is of type %T \n", username)
 
+	var anothervariable int
+	fmt.Println(anothervariable) // instead of any garbage value like in other programming languages it returns zero
+
+	var anotherstring string
+	fmt.Println(anotherstring) // it returns blank space for strings
+
 }
