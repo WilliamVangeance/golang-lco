307f1d467b2f8174b2b12ee5b5b8fc635bb5d421
google <kabirsingh@gmail.com>
2025-03-17 11:21:25 +0530

    going with the basics

--- 02variables/main.go
+++ 02variables/main.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var isequal bool = true
+	var isequal uint8 = 255 // unsigned integer, containes 0-255 , similiarily we have uint16 , uint32 etc.,
 	fmt.Println(isequal)
 	fmt.Printf("vairable is of type %T \n", isequal)
+
+	var username string = "yugal"
+	fmt.Println(username)
+	fmt.Printf("variable username is of type %T \n", username)
 
 }
