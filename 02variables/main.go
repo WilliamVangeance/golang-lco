8f45221c6ed8ea7616ff5fa6b7cb1b7339775e5f
google <kabirsingh@gmail.com>
2025-03-17 11:11:07 +0530

    going with the basics

--- 02variables/main.go
+++ 02variables/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var username string = "yugal"
-	fmt.Println(username)
-	fmt.Printf("vairable is of type %T \n", username)
+	var isequal bool = true
+	fmt.Println(isequal)
+	fmt.Printf("vairable is of type %T \n", isequal)
 
 }
